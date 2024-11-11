4cf7aac41d5780dd7832af3865df383a6d608e60
Bito-Zalan <bitozalan23@gmail.com>
2024-11-11 09:32:20 +0100

    Feladatok main-ben.

---
+++ src/main/java/hu/szamalk/HengerProgram.java
@@ -0,0 +1,32 @@
+package hu.szamalk;
+
+import modell.MertaniHenger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HengerProgram {
+
+    private List<MertaniHenger> hengerek;
+
+    public static void main(String[] args) {
+
+    }
+
+    public HengerProgram(){
+        hengerek = new ArrayList<>();
+    }
+    public double atlagTerfogat(){
+        return 0.0;
+    }
+
+    public double csovekSulya(){
+        return 0.0;
+    }
+    private List<MertaniHenger> lista(){
+        return new ArrayList<>();
+    }
+    public void run(){
+
+    }
+}
